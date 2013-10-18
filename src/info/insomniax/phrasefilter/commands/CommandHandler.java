08f200d191c590f68313b52c6c6beeeeb4ff1636
shadoman75 <shadowkiller77776@yahoo.com>
2013-10-17 22:29:29 -0500

    TeeHee

---
+++ src/info/insomniax/phrasefilter/commands/CommandHandler.java
@@ -0,0 +1,10 @@
+package info.insomniax.phrasefilter.commands;
+
+public class CommandHandler {
+
+}
+public final class CommandHandler extends CommandExecutor{
+	public boolean onCommand(){
+		
+	}
+}
